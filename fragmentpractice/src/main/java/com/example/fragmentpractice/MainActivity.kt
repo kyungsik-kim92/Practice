dbe2f9cf2610609b77f9dbb4763ac792f191932a
Kim Kyungsik <ksiki1@naver.com>
2023-08-22 20:48:45 +0900

    뷰페이저 따라해보기

--- fragmentpractice/src/main/java/com/example/fragmentpractice/MainActivity.kt
+++ fragmentpractice/src/main/java/com/example/fragmentpractice/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.example.fragmentpractice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import androidx.appcompat.app.AppCompatActivity
 import com.example.fragmentpractice.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private val tabConfigurationStrategy =
+        TabLayoutMediator.TabConfigurationStrategy { tab, positon ->
+            tab.icon = resources.obtainTypedArray(R.array.array_main_tab_icon).getDrawable(positon)
+            tab.text = resources.getStringArray(R.array.array_main_tab_text)[positon]
+
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -25,10 +32,13 @@ class MainActivity : AppCompatActivity() {
             Fragment4(),
         )
 
-    val pageAdapter = FragmentPagerAdapter(list,this)
+        val pageAdapter = FragmentPagerAdapter(list, this)
 
-        with(binding){
+        with(binding) {
 
+            viewPager.adapter = pageAdapter
+
+            TabLayoutMediator(tabLayout, viewPager, tabConfigurationStrategy).attach()
 
         }
 
