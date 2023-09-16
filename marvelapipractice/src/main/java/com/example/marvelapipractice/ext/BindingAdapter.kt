16cb2ba7e545e124b366b7a51d36d8a103790598
Parkduksung <duksung1234@naver.com>
2023-09-16 11:38:08 +0900

    9.16 code review 4 MediaUtil 를 이용한 이미지 로컬 저장.

--- marvelapipractice/src/main/java/com/example/marvelapipractice/ext/BindingAdapter.kt
+++ marvelapipractice/src/main/java/com/example/marvelapipractice/ext/BindingAdapter.kt
@@ -1,14 +1,16 @@
 package com.example.marvelapipractice.ext
 
 import android.widget.ImageView
+import androidx.core.view.drawToBitmap
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
+import com.example.marvelapipractice.util.MediaUtil.Companion.saveToGallery
 
 @BindingAdapter("setUrlImg")
-fun ImageView.setUrlImg(url : String?) {
+fun ImageView.setUrlImg(url: String?) {
     Glide.with(context)
         .load(url)
         .apply(RequestOptions.bitmapTransform(RoundedCorners(15)))
@@ -17,9 +19,18 @@ fun ImageView.setUrlImg(url : String?) {
 
 }
 
+@BindingAdapter("saveToGalleryForClickImage")
+fun ImageView.saveToGalleryForClickImage(isUse: Boolean?) {
+    if (isUse == true) {
+        setOnClickListener {
+            drawToBitmap().saveToGallery(context)
+        }
+    }
+}
+
 
 @BindingAdapter("onScrolledBottomLine")
-fun RecyclerView.onScrolledBottomLine(f : Function1<Boolean, Unit>?) {
+fun RecyclerView.onScrolledBottomLine(f: Function1<Boolean, Unit>?) {
     this.addOnScrollListener(object : RecyclerView.OnScrollListener() {
         override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
             super.onScrolled(recyclerView, dx, dy)
--- marvelapipractice/src/main/java/com/example/marvelapipractice/ui/adapter/MarvelViewHolder.kt
+++ marvelapipractice/src/main/java/com/example/marvelapipractice/ui/adapter/MarvelViewHolder.kt
@@ -1,12 +1,14 @@
 package com.example.marvelapipractice.ui.adapter
 
 import android.util.Log
+import androidx.core.view.drawToBitmap
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
 import com.example.marvelapipractice.databinding.ItemCharacterBinding
 import com.example.marvelapipractice.network.response.Result
+import com.example.marvelapipractice.util.MediaUtil.Companion.saveToGallery
 
 class MarvelViewHolder(private val binding: ItemCharacterBinding) :
     RecyclerView.ViewHolder(binding.root) {
@@ -15,5 +17,4 @@ class MarvelViewHolder(private val binding: ItemCharacterBinding) :
     fun bind(item: Result) {
         binding.result = item
     }
-
 }
