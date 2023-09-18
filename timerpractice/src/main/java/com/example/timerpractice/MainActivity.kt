690a514413c772bf2fe891f4bc6dc4e7d952c200
Kim Kyungsik <ksiki1@naver.com>
2023-09-18 15:34:57 +0900

    버튼을 1번 눌렀을 때 이상 없지만 다시 눌렀을 때 뒤죽박죽 현상이 생김

--- timerpractice/src/main/java/com/example/timerpractice/MainActivity.kt
+++ timerpractice/src/main/java/com/example/timerpractice/MainActivity.kt
@@ -2,10 +2,48 @@ package com.example.timerpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.timerpractice.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    private val coroutineScopeMainImmediate = CoroutineScope(Dispatchers.Main.immediate)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initViews()
     }
+
+    private fun initViews() {
+
+
+        binding.btnStart.setOnClickListener {
+
+            coroutineScopeMainImmediate.launch {
+                binding.tvStatus.text = "start"
+
+
+                runTime()
+
+                binding.tvStatus.text = "ended"
+
+            }
+        }
+    }
+
+    private suspend fun runTime() {
+        var seconds = 10
+        while (seconds >= 0) {
+            binding.count.text = "${seconds--}"
+            delay(1000)
+        }
+    }
+
+
 }
