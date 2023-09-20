ac48c500f68477abd6e4592da5b0d9034d26ae7f
Kim Kyungsik <ksiki1@naver.com>
2023-09-20 15:36:48 +0900

    버튼이 눌리면 다시 처음부터 10이 시작되게끔 수정

--- timerpractice/src/main/java/com/example/timerpractice/MainActivity.kt
+++ timerpractice/src/main/java/com/example/timerpractice/MainActivity.kt
@@ -1,15 +1,11 @@
 package com.example.timerpractice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.CountDownTimer
-import android.util.Log
-import androidx.lifecycle.ViewModelProvider
-import com.example.timerpractice.MainViewModel.Companion.MIllIS_IN_FUTURE
-import com.example.timerpractice.MainViewModel.Companion.TICK_INTERVAL
+import androidx.appcompat.app.AppCompatActivity
 import com.example.timerpractice.databinding.ActivityMainBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -18,30 +14,50 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     //    private val coroutineScopeMainImmediate = CoroutineScope(Dispatchers.Main.immediate)
-    private lateinit var timer: CountDownTimer
+//    private lateinit var timer: CountDownTimer
+    private var job: Job? = null
 
-    private val viewModel: MainViewModel by lazy {
-        ViewModelProvider(this)[MainViewModel::class.java]
-    }
+//    private val viewModel: MainViewModel by lazy {
+//        ViewModelProvider(this)[MainViewModel::class.java]
+//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 //        initViews()
-//        setUpCountDownTimer()
-        binding.btnStart.setOnClickListener {
-            viewModel.customTimerDuration.observe(this) {
-                binding.count.text = removeLastNchars(it.toString(), 3)
-                viewModel.timerJob.start()
+////        setUpCountDownTimer()
+//        binding.btnStart.setOnClickListener {
+//            viewModel.customTimerDuration.observe(this) {
+//                binding.count.text = removeLastNchars(it.toString(), 3)
+//                viewModel.timerJob.start()
+//
+//            }
+//        }
 
+        binding.btnStart.setOnClickListener {
+
+            job?.cancel()
+            job = CoroutineScope(Dispatchers.Main).launch {
+                for (i in 10 downTo 0) {
+                    binding.count.text = "$i"
+                    delay(1000) // 1초 대기
+                }
+                binding.count.text = "시간 종료!"
             }
         }
     }
+override fun onDestroy() {
+    super.onDestroy()
+    job?.cancel()
+}
 
-    private fun removeLastNchars(str: String, n: Int): String {
-        return str.substring(0, str.length - n)
-    }
+}
+
+
+//private fun removeLastNchars(str: String, n: Int): String {
+//    return str.substring(0, str.length - n)
+//}
 //    private fun setUpCountDownTimer() {
 //        timer = object : CountDownTimer(MIllIS_IN_FUTURE, TICK_INTERVAL) {
 //            override fun onTick(millisUntilFinished: Long) {
@@ -78,4 +94,8 @@ class MainActivity : AppCompatActivity() {
 //    }
 
 
-}
+
+
+
+
+
