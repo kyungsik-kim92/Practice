ea360c872744dbf6d2a8d6cbfe5e9785e6e3edc6
Kim Kyungsik <ksiki1@naver.com>
2023-09-25 18:23:48 +0900

    timer 따라해보기

--- timerpractice/src/main/java/com/example/timerpractice/MainActivity.kt
+++ timerpractice/src/main/java/com/example/timerpractice/MainActivity.kt
@@ -14,11 +14,6 @@ import kotlinx.coroutines.launch
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
-    //    private val coroutineScopeMainImmediate = CoroutineScope(Dispatchers.Main.immediate)
-//    private lateinit var timer: CountDownTimer
-//    private var job: Job? = null
-
     private val viewModel: MainViewModel by lazy {
         ViewModelProvider(this)[MainViewModel::class.java]
     }
@@ -27,77 +22,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-//        initViews()
-////        setUpCountDownTimer()
-//        binding.btnStart.setOnClickListener {
-//            viewModel.customTimerDuration.observe(this) {
-//                binding.count.text = removeLastNchars(it.toString(), 3)
-//                viewModel.timerJob.start()
-//
-//            }
-//        }
-
-        binding.btnStart.setOnClickListener {
-            lifecycleScope.launch {
-                viewModel.resetCountdown()
-                viewModel.countdownState.collect { countdown ->
-                    binding.count.text = "$countdown"
-                    if (countdown == 0) {
-                        binding.count.text = "시간 종료!"
-                    }
-                }
-
-//            job?.cancel()
-//            job = CoroutineScope(Dispatchers.Main).launch {
-//                for (i in 10 downTo 0) {
-//                    binding.count.text = "$i"
-//                    delay(1000) // 1초 대기
-//                }
-//                binding.count.text = "시간 종료!"
-//            }
-            }
-        }
     }
 }
 
-
-//private fun removeLastNchars(str: String, n: Int): String {
-//    return str.substring(0, str.length - n)
-//}
-//    private fun setUpCountDownTimer() {
-//        timer = object : CountDownTimer(MIllIS_IN_FUTURE, TICK_INTERVAL) {
-//            override fun onTick(millisUntilFinished: Long) {
-//                viewModel.countDownTimerDuration.value = millisUntilFinished
-//            }
-//
-//            override fun onFinish() {}
-//        }
-//    }
-
-//    private fun initViews() {
-//
-//
-//        binding.btnStart.setOnClickListener {
-//
-//            coroutineScopeMainImmediate.launch {
-//                binding.tvStatus.text = "start"
-//
-//
-//                runTime()
-//
-//                binding.tvStatus.text = "ended"
-//
-//            }
-//        }
-//    }
-//
-//    private suspend fun runTime() {
-//        var seconds = 10
-//        while (seconds >= 0) {
-//            binding.count.text = "${seconds--}"
-//            delay(1000)
-//        }
-//    }
 
 
 
--- timerpractice/src/main/java/com/example/timerpractice/MainViewModel.kt
+++ timerpractice/src/main/java/com/example/timerpractice/MainViewModel.kt
@@ -1,30 +1,61 @@
 package com.example.timerpractice
 
+import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.cancellable
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
 
-    private val _countdownState = MutableStateFlow(10)
-    val countdownState: StateFlow<Int> = _countdownState
+    val timerProperty = TimerProperty()
 
-    init {
-        startCountdown()
-    }
+    private lateinit var timerJob: Job
 
-    private fun startCountdown() {
-        viewModelScope.launch {
-            while (_countdownState.value > 0) {
-                kotlinx.coroutines.delay(1000)
-                _countdownState.value -= 1
+    fun startTimer() {
+        if (::timerJob.isInitialized)
+            timerJob.cancel()
+
+        timerProperty.clear()
+
+        timerJob = flow {
+            while (timerProperty.time > 0) {
+                delay(1000L)
+                emit(timerProperty.time--)
             }
+        }.cancellable().launchIn(viewModelScope)
+    }
+
+    class TimerProperty : BaseObservable() {
+        @get:Bindable
+        var time: Int = INIT_TIME
+            set(value) {
+                field = value
+                notifyPropertyChanged(BR.time)
+            }
+
+        @get:Bindable
+        var message: String = EMPTY_MESSAGE
+            set(value) {
+                field = value
+                notifyPropertyChanged(BR.message)
+            }
+
+        fun clear() {
+            time = INIT_TIME
+            message = EMPTY_MESSAGE
         }
 
-    }
-        fun resetCountdown() {
-            _countdownState.value = 10
+        companion object {
+            private const val INIT_TIME = 10
+            private const val EMPTY_MESSAGE = ""
         }
+    }
 }
